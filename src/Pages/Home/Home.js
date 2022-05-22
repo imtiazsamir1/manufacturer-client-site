767d9279b8164ae89a0a0f82f6f559713c2f1a91
imtiazsamir1 <imtiazsamir416@gmail.com>
2022-05-22 21:20:06 +0600

    banner

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Banner from "./Banner";
+import Cards from "./Cards";
+import Parts from "./Parts";
+
+const Home = () => {
+  return (
+    <div>
+      <Banner></Banner>
+      <Parts></Parts>
+      <Cards></Cards>
+    </div>
+  );
+};
+
+export default Home;

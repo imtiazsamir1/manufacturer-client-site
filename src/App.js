208e3699386a1846871f8625be2280a9a13a816e
imtiazsamir1 <imtiazsamir416@gmail.com>
2022-05-27 15:23:20 +0600

    payment

--- src/App.js
+++ src/App.js
@@ -1,99 +1,121 @@
-import logo from "./logo.svg";
-import "./App.css";
 import { Route, Routes } from "react-router-dom";
-
-import Home from "./Pages/Home/Home";
-import NavBer from "./Pages/Sheared/NavBer";
-import Footer from "./Pages/Sheared/Footer";
-import Login from "./Pages/Sheared/Login/Login";
-import SignUp from "./Pages/Sheared/Login/SignUp";
-import MyOrders from "./Pages/MyOrders/MyOrders";
-import RequireAuth from "./Pages/Sheared/Login/RequireAuth";
-import PartsDetail from "./Pages/PartsDetail/PartsDetail";
-import Dashboard from "./Pages/Dashboard/Dashboard";
-import MyProfile from "./Pages/Dashboard/MyProfile";
-import MyOrder from "./Pages/Dashboard/MyOrder";
-import AddReview from "./Pages/Dashboard/AddReview";
-import Purchase from "./Pages/Purchase/Purchase";
-import Blogs from "./Pages/Blogs/Blogs";
-import ManageProducts from "./Pages/ManageProducts/ManageProducts";
-import NotFound from "./Pages/ManageProducts/NotFound/NotFound";
-import AddProduct from "./Pages/Dashboard/AddProduct/AddProduct";
-import ChackOut from "./Pages/ChackOut/ChackOut";
-import MyPortfolio from "./Pages/ChackOut/Protfolio/MyProtfolio";
-import ManageAllOrder from "./Pages/ManageAllOrder/ManageAllOrder";
+import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
+import Footer from "./components/Footer/Footer";
+import Login from "./components/Login/Login";
+import Home from "./components/Navbar/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
+import Signup from "./components/Signup/Signup";
+import RequireAuth from "./RequireAuth";
+import CheckOutPage from "./components/CheckOutPage/CheckOutPage";
+import PurchaseSinglePage from "./components/PurchaseSinglePage/PurchaseSinglePage";
+import MyOrders from "./components/MyOrders/MyOrders";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
-import User from "./Pages/User/User";
-import Banner from "./Pages/Home/Banner";
-import Review from "./Pages/Review/Review";
+import AddReview from "./components/AddReview/AddReview";
+import MyProfile from "./components/Navbar/Home/MyProfile/MyProfile";
+import { ToastContainer } from "react-toastify";
+import Dashboard from "./components/Dashboard/Dashboard";
+import User from "./components/User/User";
+import RequireAdmin from "./components/Dashboard/RequireAdmin";
+import AddProduct from "./components/AddProduct/AddProduct";
+import ManageAllOrders from "./components/ManageAllOrders/ManageAllOrders";
+import ManageProducts from "./components/ManageProducts/ManageProducts";
+import Blogs from "./components/Blogs/Blogs";
+import NotFound from "./components/NotFound/NotFound";
+import MyPortfolio from "./components/MyPortfolio/MyPortfolio";
 const stripePromise = loadStripe(
-  "pk_test_51L0j5UHw1ppVQWndFtGKSCw5rDU15PD6vBHX3o3Oi9OAtC6BF1Xak8n06YD4S8LxEl78IpklBsg7ZGrfku1vAymo00XYkGq7Yt"
+  "pk_test_51L3yjfHAMO91AfoW7RESLgI8tRjjNb7J3Zvyiu1wClMslzWu5Je4r1KEXwFFNx4LBcLyjawkWsAJTJNRdi3IBUpy00LbtOKN1D"
 );
-
 function App() {
   return (
     <div>
-      <NavBer></NavBer>
-
+      <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
-
+        <Route path="login" element={<Login></Login>}></Route>
         <Route
-          className="max-w-7xl mx-auto px-12"
-          path="/blogs"
-          element={<Blogs></Blogs>}
+          path="my-portfolio"
+          element={<MyPortfolio></MyPortfolio>}
         ></Route>
-        <Route path="/purchase" element={<Purchase></Purchase>}></Route>
+        <Route path="signup" element={<Signup></Signup>}></Route>
+        <Route path="blogs" element={<Blogs></Blogs>}></Route>
 
-        <Route
-          path="/purchase/:id"
-          element={<PartsDetail></PartsDetail>}
-        ></Route>
         <Route
           path="dashboard"
           element={
             <RequireAuth>
-              <Dashboard />
+              <Dashboard></Dashboard>
             </RequireAuth>
           }
         >
-          <Route index element={<User></User>}></Route>
-          <Route path="addreview" element={<AddReview></AddReview>}></Route>
-          <Route path="protfolio" element={<MyPortfolio></MyPortfolio>}></Route>
-          <Route path="addproduct" element={<AddProduct></AddProduct>}></Route>
-          <Route path="myorders" element={<MyOrders></MyOrders>}></Route>
-          <Route path="user" element={<User></User>}></Route>
           <Route
-            path="manageall"
-            element={<ManageAllOrder></ManageAllOrder>}
+            path="my-profile"
+            element={
+              <RequireAuth>
+                <MyProfile></MyProfile>
+              </RequireAuth>
+            }
           ></Route>
-
+          <Route index element={<MyOrders></MyOrders>}></Route>
+          <Route path="my-orders" element={<MyOrders></MyOrders>}></Route>
+          <Route path="add-review" element={<AddReview></AddReview>}></Route>
           <Route
-            path="manageproducts"
-            element={<ManageProducts></ManageProducts>}
+            path="user"
+            element={
+              <RequireAdmin>
+                <User></User>
+              </RequireAdmin>
+            }
+          ></Route>
+          <Route
+            path="add-product"
+            element={
+              <RequireAdmin>
+                <AddProduct></AddProduct>
+              </RequireAdmin>
+            }
+          ></Route>
+          <Route
+            path="manage-all-orders"
+            element={
+              <RequireAdmin>
+                <ManageAllOrders></ManageAllOrders>
+              </RequireAdmin>
+            }
+          ></Route>
+          <Route
+            path="manage-products"
+            element={
+              <RequireAdmin>
+                <ManageProducts></ManageProducts>
+              </RequireAdmin>
+            }
           ></Route>
         </Route>
+        <Route
+          path="purchase/:id"
+          element={
+            <RequireAuth>
+              <PurchaseSinglePage></PurchaseSinglePage>
+            </RequireAuth>
+          }
+        ></Route>
 
         <Route
           path="checkout/:paymentID"
           element={
             <RequireAuth>
               <Elements stripe={stripePromise}>
-                <ChackOut />
+                <CheckOutPage />
               </Elements>
             </RequireAuth>
           }
         ></Route>
-
-        <Route path="/*" element={<NotFound></NotFound>}></Route>
-        <Route path="/review" element={<Review></Review>}></Route>
-        <Route path="/order" element={<MyOrder></MyOrder>}></Route>
-        <Route path="/signup" element={<SignUp></SignUp>}></Route>
-        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
-
       <Footer></Footer>
+      <ToastContainer></ToastContainer>
     </div>
   );
 }
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,10 +1,18 @@
-import { getAuth } from "firebase/auth";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_apiKey,
+//     authDomain: process.env.REACT_APP_authDomain,
+//     projectId: process.env.REACT_APP_projectId,
+//     storageBucket: process.env.REACT_APP_storageBucket,
+//     messagingSenderId: process.env.REACT_APP_messagingSenderId,
+//     appId: process.env.REACT_APP_appId,
+// };
 const firebaseConfig = {
   apiKey: "AIzaSyAKcroCEhiZCqx5YIwsZWVzbNB3BYCcxCg",
   authDomain: "bike-parts-5e55f.firebaseapp.com",
@@ -16,7 +24,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 const auth = getAuth(app);
-
 export default auth;
